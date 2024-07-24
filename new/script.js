4c415133c4af8ce2726d5264c97a3dd50aa18890
Jeba Ansari <ansarijeba61@gmail.com>
2024-07-24 23:40:52 +0530

    first commit

---
+++ new/script.js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementById("navLinks");
+var nav = document.getElementById("nav");
+function showMenu(){
+    nav.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
