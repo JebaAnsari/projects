5b7cc6ced5feb52c729adc50f37f96920f2d72c1
Jeba Ansari <ansarijeba61@gmail.com>
2024-07-26 00:23:59 +0530

    new change

---
+++ new/02.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert(3+3); we are using nodeJs
+// console.log(typeof "jeba");
+// console.log(typeof null);
+
+let number = "33";
+let newNumber = Boolean(number);
+console.log(typeof newNumber);
+
+
+const Array  = ["Jeba","Ansari",34];
+let obj ={
+    name :"htesh",
+    age:23,
+}
+
+console.log(Array);
+function hello(){
+    console.log(Array);
+}
