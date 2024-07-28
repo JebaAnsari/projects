4c52995062058376ada0c1a486c98ac820d95873
Jeba Ansari <114054093+JebaAnsari@users.noreply.github.com>
2024-07-28 22:42:54 +0530

    Delete new directory

--- new/test.js
+++
@@ -1,7 +0,0 @@
-const accountId = 1233;
-let accountEmail  = "Jeba@gmail.com";
-var accountnumber  = "1243";
-
-accountCity = "Jaipur";
-let accountState;
-console.table([accountEmail,accountId,accountState,accountCity,accountnumber]);
